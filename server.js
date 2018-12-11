4c5baa91995fe6ff23dc6a49ac20d6e78a90d54f
Deziree Teague <dez.teague@gmail.com>
2018-12-11 10:34:56 -0800

    finished rendering server side

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+const superagent = require('superagent');
+const app = express();
+
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+
+app.use(express.urlencoded({extended: true}));
+// app.use(express.static('./public'));
+
+app.set('view engine', 'ejs');
+
+app.get('/hello', (req, res) => {
+  res.render('../views/pages/index');
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on PORT: ${PORT}`);
+});
