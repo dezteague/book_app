3b3d8f95bc5ece0a67f289b5eed64a6e9a6f20be
Deziree Teague <dez.teague@gmail.com>
2018-12-12 09:51:15 -0800

    updated features 3 and 4

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(express.static('./public'));
 const client = new Client(process.env.DATABASE_URL);
 
 client.connect();
+// error handler
 client.on('error', err => console.error(err));
 
 //set template
@@ -30,30 +31,55 @@ app.get('/', (req, res) => {
   res.render('../views/pages/index');
 });
 
-app.post('/searches', getBook);
+app.post('/search', getBook);
 
 //helper function
 function getBook(req, res) {
   const bookHandler = {
     query: req.body.search_query,
-    queryType: req.body.title === 'on' ? intitle : inauthor,
-  }
+    queryType: req.body.title === 'on' ? 'intitle' : 'inauthor',
+  };
+  //grab API data if the database is empty
+  Book.fetch(bookHandler, res);
 }
 
 //book constructor function
-function Book(query, data) {
-  this.search_query = query;
+function Book(data) {
   this.title = data.volumeInfo.title;
   this.author = data.volumeInfo.authors;
   this.description = data.volumeInfo.description;
   this.image = data.imageLinks.thumbnail;
 }
 
-let book = new Book(query.data.body.items);
+Book.fetch = function (bookHandler, res) {
+  //fetch data from google books API
+  const url = `https://www.googleapis.com/books/v1/volumes?q=${bookHandler.queryType}:${bookHandler.query}`;
+  superagent.get(url)
+    .then(results => {
+      let bookArray = Book.createBooks(results.body.items);
+      return bookArray;
+    })
+    .then (results => {
+      res.render('./pages/searches/show', { allBooks: results});
+    });
+};
 
-//proxy request to Google Books API
-const url = `https://www.googleapis.com/books/v1/volumes?q=${query}`;
-return superagent.get(url)
+Book.createBooks = function (bookInfo) {
+  //this makes an array of books that will be rendered on the page
+  let allBooks = [];
+  if (bookInfo.length < 1) {
+    return allBooks;
+  } else {
+    if (bookInfo.length > 10) {
+      bookInfo = bookInfo.slice [0,10];
+    }
+    allBooks = bookInfo.map (item => {
+      let book = new Book (item);
+      return book;
+    });
+    return allBooks;
+  }
+};
 
 app.listen(PORT, () => {
   console.log(`listening on PORT: ${PORT}`);
